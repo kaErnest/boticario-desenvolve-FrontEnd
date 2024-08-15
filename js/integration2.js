ef5d3a215a64c26344d8530dcc092cdf44927068
Karoline Ernest <ernest.karoline@gmail.com>
2024-08-15 16:36:12 -0300

    feat(kits): :sparkles: monta kits dinamicamente

--- js/integration2.js
+++ js/integration2.js
@@ -14,60 +14,46 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    // Chama a função para buscar e renderizar os kits
+    fetchKits();
+
     function renderKits(kits) {
         kitsContainer.innerHTML = '';
     
         kits.forEach(kit => {
-            fetchProductsForKit(kit.item).then(products => {
-                const kitElement = `
-                    <div class="product">
-                        <div class="product-heart">
-                            <i class="fa-solid fa-heart"></i>
-                        </div>
-                        
-                        <img src="http://localhost:3000/${kit.image.path}" class="product-image" alt="${kit.titulo}" width="256" height="256">
-                        
-                        <h3 class="product-title">${kit.titulo}</h3>
-                        
-                        <span class="product-description">
-                            ${products.map(product => `${product} 1 ${product.nome}: ${product.descricao}`).join('<br>')}
-                        </span>
-    
-                        <div class="product-rate">
-                            <i class="fa-solid fa-star"></i>
-                            <i class="fa-solid fa-star"></i>
-                            <i class="fa-solid fa-star"></i>
-                            <i class="fa-solid fa-star"></i>
-                            <i class="fa-solid fa-star"></i>
-                            <span>(500+)</span>
-                        </div>
-    
-                        <div class="product-price">
-                            <h4>R$${kit.preco.toFixed(2)}</h4>
-                            <button class="btn-default add-to-cart" data-name="${kit.titulo}" data-price="${kit.preco}">
-                                <i class="fa-solid fa-basket-shopping"></i>
-                            </button>
-                        </div>
+            const kitElement = `
+                <div class="product">
+                    <div class="product-heart">
+                        <i class="fa-solid fa-heart"></i>
                     </div>
-                `;
-                kitsContainer.innerHTML += kitElement;
-            });
+                    
+                    <img src="http://localhost:3000/${kit.image.path}" class="product-image" alt="${kit.titulo}" width="256" height="256">
+                    
+                    <h3 class="product-title">${kit.titulo}</h3>
+                    
+                    <span class="product-description">
+                        ${kit.item.map(product => `1 ${product.nome}: ${product.descricao}`).join('<br>')}
+                    </span>
+
+                    <div class="product-rate">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <span>(500+)</span>
+                    </div>
+
+                    <div class="product-price">
+                        <h4>R$${kit.preco.toFixed(2)}</h4>
+                        <button class="btn-default add-to-cart" data-name="${kit.titulo}" data-price="${kit.preco}">
+                            <i class="fa-solid fa-basket-shopping"></i>
+                        </button>
+                    </div>
+                </div>
+            `;
+            kitsContainer.innerHTML += kitElement;
         });
     }
-
-    // Função para buscar produtos com base nos IDs
-    async function fetchProductsForKit(productIds) {
-        try {
-            const responses = await Promise.all(productIds.map(id => fetch(`http://localhost:3000/itens/${id}`)));
-            const products = await Promise.all(responses.map(response => response.json()));
-            return products;
-        } catch (error) {
-            console.error('Erro ao buscar os produtos para o kit:', error);
-            return [];
-        }
-    }
-
-    // Chama a função para buscar e renderizar os kits
-    fetchKits();
 });
 
